cd9cb0c839b47ae1a6b389b6c1a78e32b7b68ecc
success7121995 <success7121995@gmail.com>
2024-11-21 22:58:10 +0800

    Update: Set Ctrl + K to focus search bar

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FormEvent, useRef, useState } from 'react';
+import { FormEvent, useRef, useState, useEffect } from 'react';
 
 // SVG
 import SearchIcon from '@/src/public/svg/search.svg';
@@ -9,6 +9,23 @@ import FilterIcon from '@/src/public/svg/filter.svg';
 const SearchBar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const searchRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    /**
+     * Press ctrl + k to focus the search bar
+     * @param e Keyboard event
+     */
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key.toLowerCase() === 'k') {
+        e.preventDefault();
+
+        searchRef.current?.focus();
+      }
+    }
+
+    window.addEventListener('keydown', e => handleKeyDown(e));
+    return () => window.removeEventListener('keydown', e => handleKeyDown(e));
+  }, []);
 
   /**
    * Handle the search submit
@@ -31,17 +48,22 @@ const SearchBar = () => {
     <div className="px-3 flex justify-between items-center w-4/6 mx-auto bg-[#F6F8FA] h-[36px] rounded-[10px]">
     
       {/* Search Bar */}
-      <form onSubmit={e => handleSearch(e)} className="flex items-center gap-x-[3px]">
-        <label>
-          <SearchIcon />
-        </label>
+      <div className="flex justify-between items-center gap-x-[5px]">
+        <form onSubmit={e => handleSearch(e)} className="flex items-center gap-x-[3px]">
+          <label>
+            <SearchIcon />
+          </label>
 
-        <input
-          ref={searchRef}
-          type="text"
-          className="font-publicSans text-xs bg-transparent text-[#C0C7D0] outline-none placeholder:font-publicSans placeholder:text-[#C0C7D0]"
-          placeholder="Search..."/>
-      </form>
+          <input
+            ref={searchRef}
+            type="text"
+            className="font-publicSans text-xs bg-transparent text-[#C0C7D0] outline-none placeholder:font-publicSans placeholder:text-[#C0C7D0]"
+            placeholder="Search..."/>
+        </form>
+        
+        {/* Tips for CtrlK */}
+        <p className="p-[2px] rounded-sm bg-[#C0C6C9] font-publicSansItalic text-[10px] text-white">CtrlK</p>
+      </div>
 
       <div className="relative">
         {/*  */}
