3ae695ea3d310f1678413f78f4049970925f1e81
success7121995 <success7121995@gmail.com>
2024-11-28 17:33:20 +0800

    Update: Working on the form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,11 +2,11 @@
 
 import { useEffect, useState } from 'react';
 import { useData, type CategoryProps } from '@/src/context/DataProvider';
-import {Select, SelectSection, SelectItem} from "@nextui-org/select";
-import { useForm, SubmitHandler } from "react-hook-form";
+import {Select, SelectItem} from "@nextui-org/select";
+import { useForm, Controller, type SubmitHandler } from "react-hook-form";
 
-interface formDataProps {
-  cateogry: string,
+interface FormDataProps {
+  category: string,
   is_important: boolean,
   title: string,
   content?: string
@@ -17,8 +17,15 @@ const Form = () => {
 
   const { fetchCategories } = useData();
 
-  const { register, handleSubmit } = useForm<formDataProps>();
-  const onSubmit: SubmitHandler<formDataProps> = data => console.log(data);
+  const { control, handleSubmit } = useForm<FormDataProps>({
+    defaultValues: {
+      category: '',
+      is_important: false,
+      title: '',
+      content: '',
+    },
+  });
+  const onSubmit: SubmitHandler<FormDataProps> = data => console.log(data);
 
   useEffect(() => {
     const handleFetchCategories = async () => {
@@ -34,19 +41,36 @@ const Form = () => {
   return (<>
     <form>
       <div>
-        <Select
-          label="Select a category"
-          classNames={{
-            base: "w-[200px]",
-            trigger: "h-[20px]",
-          }}
-        >
-          {categories.map(categories => (
-            <SelectItem key={categories.c_id} textValue={categories.c_name}>
-              <p className="text-xs font-publicSans text-darkText">{categories.c_name}</p>
-            </SelectItem>
-          ))}
-        </Select>
+        <Controller
+          name="category"
+          control={control}
+          render={({ field, fieldState}) => (
+            <>
+              <Select
+                { ...field }
+                placeholder="Select a category"
+                onChange={value => field.onChange(value)}
+                classNames={{
+                  base: 'w-[200px] h-[10px]',
+                  trigger: 'h-[10px]',
+                  label: 'text-xs font-publicSans text-disableText',
+                }}
+              >
+                {categories.map(categories => (
+                  <SelectItem key={categories.c_id} textValue={categories.c_name}>
+                    <p className="text-xs font-publicSans text-darkText">{categories.c_name}</p>
+                  </SelectItem>
+                ))}
+              </Select>
+
+              {fieldState.error && (
+                <p className="mt-1 text-xs text-red-600">
+                  {fieldState.error.message}
+                </p>
+              )}
+            </>
+          )}
+        />    
       </div>
 
     </form>
